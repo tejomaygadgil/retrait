d309305bac0222b351a21642d1c7b66d95323012
tejomaygadgil <tejomaygadgil@gmail.com>
2023-12-09 23:33:07 -0800

    Add Link and Browser class

--- retrait.py
+++ retrait.py
@@ -1,53 +1,76 @@
 class Link:
-    def __init__(self, msg, parent) -> None:
+    def __init__(self, msg, parent=None) -> None:
         self.msg = msg
         self.parent = parent
         self.children = []
 
     def __repr__(self):
-        return f"{self.msg}, parent='{self.parent.msg}', {len(self.children)} children"
+        return f"msg='{self.msg}'"
 
     def add(self, msg):
-        tab = Link(msg, self)
-        self.children.append(tab)
+        link = Link(msg, self)
+        self.children.append(link)
 
 
-def open_url(link, url, return_url):
-    """
-    Add url to Link and return new Link.
-    """
-    link.add(url)
-    if return_url:
-        return link.children[-1]
+class Browser:
+    def __init__(self):
+        self.tabs = []
+        self.head = None
 
+    def __repr__(self):
+        return f"i={self.head}, tabs={self.tabs}"
 
-def go_back(link):
-    """
-    Return parent Link.
-    """
-    return link.parent
+    def get_head(self):
+        return self.tabs[self.head]
 
+    def set_head(self, link):
+        self.tabs[self.head] = link
 
-def get_tabs(link):
-    """
-    Return leaf Links.
-    """
-    if not link.children:
-        return link
-    else:
-        for child in link.children:
-            get_tabs(link)
+    def open_new(self, msg):
+        link = Link(msg)
+        self.tabs.append(link)
+        self.head = len(self.tabs) - 1
+
+    def forward(self, msg):
+        head = self.get_head()
+        head.add(msg)
+        self.set_head(head.children[-1])
+
+    def backward(self):
+        head = self.get_head()
+        self.set_head(head.parent)
+
+    def open_tab(self, msg):
+        head = self.get_head()
+        head.add(msg)
+        self.tabs.append(head.children[-1])
+
+    def switch_tab(self, msg):
+        for i, tab in enumerate(self.tabs):
+            if tab.msg == msg:
+                self.head = i
+
+    def close_tab(self, msg):
+        for i, tab in enumerate(self.tabs):
+            if tab.msg == msg:
+                del self.tabs[i]
 
 
 if __name__ == "__main__":
-    urls = ["Googled something", "Link 1" "Link 2" "Link 3" "Googled other thing"]
+    urls = [
+        "Googled something",
+        "Link 1",
+        "Link 2",
+        "Link 3",
+        "Googled other thing",
+    ]
 
-    t = Link(urls[0], None)  # Start: Google something
-    # Open: Link 1
-    t = open_url(t, urls[1], return_url=True)
-    t = go_back(t)  # Go back
-    open_url(t, urls[2], return_url=False)  # New tab: Link 2
-    open_url(t, urls[3], return_url=False)  # New tab: Link 3
-    pass  # Switch tab: Link 2
-    t.add_child(4)  # Open: Google y
-    pass  #
+    browser = Browser()
+
+    browser.open_new(urls[0])
+    browser.forward(urls[1])
+    browser.backward()
+    browser.open_tab(urls[2])
+    browser.open_tab(urls[3])
+    browser.switch_tab(urls[2])
+    browser.forward(urls[4])
